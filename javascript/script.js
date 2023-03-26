0276e9c0e72e6d56400ca04b47a81f1ce0afb4bd
THG20203 <tristanhgriffiths8@yahoo.com>
2023-03-26 21:54:56 +0100

    javascript for card panels

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,23 @@
 'use script';
 
-// video player javascript
+//players cards
+const panels = document.querySelectorAll('.panel');
+// querySelector all putting all panels into a node list
+
+panels.forEach((panel) => {
+  // want an event listener on each panel so if we click on it something happens
+  panel.addEventListener('click', () => {
+    /* before we add a class of active - remove the class of active on all the others 
+    so we're going to use a function called removeActiveClasses we'll create below */
+    removeActiveClasses();
+    /* panel.classList gives me a list of the classes, but also have methods
+    on the classList like 'add', can specifically add a class */
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
